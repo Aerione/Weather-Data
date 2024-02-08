99f6dbeaab38d8d8813fcf307ca79156ebb654fd
Archieturbo <archieturbo@live.se>
2024-02-08 09:33:47 +0100

    gjorde en meny

---
+++ Väderdata - Inlämning/Meny.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Väderdata___Inlämning;
+
+namespace Meny
+{
+    internal class Meny
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "../../../Files/tempData5.txt";
+
+
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("Välkommen till väderapplikationen!");
+                Console.WriteLine("Välj en funktion:");
+                Console.WriteLine("1. Medeltemperatur och medelluftfuktighet per dag, för valt datum");
+                Console.WriteLine("2. Sortering av varmast till kallaste dagen enligt medeltemperatur per dag");
+                Console.WriteLine("3. Sortering av torrast till fuktigaste dagen enligt medelluftfuktighet per dag");
+                Console.WriteLine("4. Sortering av minst till störst risk av mögel");
+                Console.WriteLine("5. Datum för meteorologisk Höst");
+                Console.WriteLine("6. Datum för meteorologisk vinter (OBS Mild vinter!)");
+                Console.WriteLine("7. Avsluta programmet");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine("Ange ett datum (ÅÅÅÅ-MM-DD): ");
+                        string inputDate = Console.ReadLine();
+
+                        break;
+                    case "2":
+
+                        break;
+                    case "3":
+
+                        break;
+                    case "4":
+
+                        break;
+                    case "5":
+
+                        break;
+                    case "6":
+
+                        break;
+                    case "7":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Ogiltigt val. Var vänlig välj ett giltigt alternativ.");
+                        break;
+                }
+            }
+        }
+
+
+        public void ShowAverageTemperatureAndHumidityForDate(DateTime chosenDate)
+        {
+
+        }
+
+
+
+        public static void SortByTemperature(List<Data> measurements)
+        {
+
+        }
+
+        public static void SortByHumidity(List<Data> measurements)
+        {
+
+        }
+
+        public static void SortByMoldRisk(List<Data> measurements)
+        {
+
+        }
+
+        public static void ShowAutumnDate(List<Data> measurements)
+        {
+
+        }
+
+        public static void ShowMildWinterDate(List<Data> measurements)
+        {
+
+        }
+
+        private static void DisplayMeasurements(List<Data> measurements)
+        {
+
+        }
+
+    }
+}
